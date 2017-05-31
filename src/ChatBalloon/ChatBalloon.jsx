acf660a2c9f8307f86e314e950d36a606b26c9e7
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-05-31 18:11:31 -0300

    Fix lint errors in ChatBalloon (WIP)

--- src/ChatBalloon/ChatBalloon.jsx
+++ src/ChatBalloon/ChatBalloon.jsx
@@ -1,39 +1,34 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
 import classnames from 'classnames';
 
 import ChatBalloonView from './ChatBalloonView';
 
-class ChatBalloon extends Component {
-  render() {
-    const {
-      headerContent,
-      bodyContent,
-      removeCaret,
-      caretRight,
-      darkTheme,
-      className,
-    } = this.props;
+const ChatBalloon = ({
+  headerContent,
+  bodyContent,
+  removeCaret,
+  caretRight,
+  darkTheme,
+  className }) => {
+  const classNames = classnames(
+    className,
+    'chat-balloon',
+    { 'is-caret-right': caretRight && !removeCaret },
+    { 'is-caret-left': !caretRight && !removeCaret },
+    { 'is-dark': darkTheme },
+  );
 
-    const classNames = classnames(
-      className,
-      'chat-balloon',
-      { 'is-caret-right': caretRight && !removeCaret },
-      { 'is-caret-left': !caretRight && !removeCaret },
-      { 'is-dark': darkTheme },
-    );
-
-    return (
-      <ChatBalloonView
-        headerContent={headerContent}
-        bodyContent={bodyContent}
-        removeCaret={removeCaret}
-        caretRight={caretRight}
-        className={classNames}
-      />
-    );
-  }
-}
+  return (
+    <ChatBalloonView
+      headerContent={headerContent}
+      bodyContent={bodyContent}
+      removeCaret={removeCaret}
+      caretRight={caretRight}
+      className={classNames}
+    />
+  );
+};
 
 ChatBalloon.propTypes = {
   headerContent: PropTypes.node.isRequired,
@@ -42,6 +37,13 @@ ChatBalloon.propTypes = {
   caretRight: PropTypes.bool,
   darkTheme: PropTypes.bool,
   className: PropTypes.string,
+};
+
+ChatBalloon.defaultProps = {
+  removeCaret: false,
+  caretRight: false,
+  darkTheme: false,
+  className: '',
 };
 
 export default ChatBalloon;
--- src/ChatBalloon/ChatBalloonView.jsx
+++ src/ChatBalloon/ChatBalloonView.jsx
@@ -1,31 +1,25 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
-class ChatBalloonView extends Component {
-  render() {
-    const {
-      headerContent,
-      bodyContent,
-      className,
-    } = this.props;
-
-    return (
-      <div className={className}>
-        <div className="chat-balloon__header">
-          { headerContent }
-        </div>
-
-        <div className="chat-balloon__body">
-          { bodyContent }
-        </div>
+const ChatBalloonView = ({
+  headerContent,
+  bodyContent,
+  className,
+  }) => (
+    <div className={className}>
+      <div className="chat-balloon__header">
+        { headerContent }
       </div>
-    );
-  }
-}
+
+      <div className="chat-balloon__body">
+        { bodyContent }
+      </div>
+    </div>
+);
 
 ChatBalloonView.propTypes = {
   headerContent: PropTypes.node.isRequired,
   bodyContent: PropTypes.node.isRequired,
-  className: PropTypes.string,
+  className: PropTypes.string.isRequired,
 };
 
 export default ChatBalloonView;
